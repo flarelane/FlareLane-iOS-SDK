29a7140adb1ea829d6dfaefbe272788e4ab6fcb5
junyeongCHOI <asxd153@gmail.com>
2021-11-22 17:11:42 +0900

    refactor FlarelaneNotification

--- FlareLane/Classes/FlareLaneNotification.swift
+++ FlareLane/Classes/FlareLaneNotification.swift
@@ -14,11 +14,61 @@ import Foundation
   public var title: String?
   public var url: String?
   
-  init(id: String, body: String, title: String?, url: String?) {
+  public init(id: String, body: String, title: String?, url: String?) {
     self.id = id;
     self.body = body;
-    self.title = title;
+    // To avoid unexpected blank lines in place of titles
+    self.title = title == "" ? nil : title;
     self.url = url;
+  }
+  
+  public func toDictionary () -> [String: Optional<String>] {
+    let data = ["id": self.id,
+                "title": self.title,
+                "body": self.body,
+                "url": self.url]
+    
+    return data
+  }
+  
+  static func getFlareLaneNotificationFromUserInfo(userInfo: [AnyHashable: Any]) -> FlareLaneNotification? {
+    guard let aps = userInfo["aps"] as? Dictionary<String, Any>,
+          let alert = aps["alert"] as? Dictionary<String, Any>,
+          let notificationId = userInfo["notificationId"] as? String,
+          let body = alert["body"] as? String else {
+            Logger.error("Failed to get FlareLaneNotification: Missing required keys")
+            return nil
+          }
+    
+    let isFlareLane = userInfo["isFlareLane"] as? Bool
+    if (isFlareLane != nil && isFlareLane != true) {
+      Logger.error("Not a notification from FlareLane.")
+      return nil
+    }
+    
+    let notification = FlareLaneNotification(id: notificationId,
+                                             body:body,
+                                             title:alert["title"] as? String,
+                                             url: alert["url"] as? String)
+    
+    return notification
+  }
+  
+  static func getFlareLaneNotificationFromLaunchOptions (launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> FlareLaneNotification? {
+    guard let userInfo = launchOptions?[.remoteNotification] as? Dictionary<String, Any>,
+          let notification = FlareLaneNotification.getFlareLaneNotificationFromUserInfo(userInfo: userInfo) else {
+            return nil
+          }
+    
+    return notification;
+  }
+  
+  static func getFlareLaneNotificationFromUNNotification(notification: UNNotification) -> FlareLaneNotification? {
+    guard let flarelaneNotification = FlareLaneNotification.getFlareLaneNotificationFromUserInfo(userInfo: notification.request.content.userInfo) else {
+      return nil
+    }
+    
+    return flarelaneNotification
   }
   
   static func isFlareLaneNotification (notification: UNNotification) -> Bool {
@@ -26,4 +76,5 @@ import Foundation
       return true
     }
     return false
-  }}
+  }
+}
