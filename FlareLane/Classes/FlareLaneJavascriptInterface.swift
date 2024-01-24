b97979f8a1f9b2e5526d46447696ea2c46df3559
JP <jpyo0613@gmail.com>
2024-01-24 18:28:49 +0900

    webview bridge 개발
    - 접근 가능하도록 available 추가
    - 줄맞춤

--- FlareLane/Classes/FlareLaneJavascriptInterface.swift
+++ FlareLane/Classes/FlareLaneJavascriptInterface.swift
@@ -5,8 +5,9 @@
 
 import WebKit
 
+@available(iOSApplicationExtension, unavailable)
 @objc class FlareLaneJavascriptInterface: NSObject, WKScriptMessageHandler {
-
+    
     private func setUserId(userId: String) {
         FlareLane.setUserId(userId: userId)
     }
@@ -27,26 +28,26 @@ import WebKit
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         guard let body = message.body as? [String: Any],
-                let method = body["method"] as? String else {
-              return
-          }
-
-          switch method {
-          case "setUserId":
-              if let userId = body["userId"] as? String {
-                  setUserId(userId: userId)
-              }
-          case "setTags":
-              if let jsonString = body["jsonString"] as? String {
-                  setTags(jsonString: jsonString)
-              }
-          case "trackEvent":
-              if let type = body["type"] as? String,
-                 let jsonString = body["jsonString"] as? String {
-                  trackEvent(type: type, jsonString: jsonString)
-              }
-          default:
-              break
-          }
+              let method = body["method"] as? String else {
+            return
+        }
+        
+        switch method {
+        case "setUserId":
+            if let userId = body["userId"] as? String {
+                setUserId(userId: userId)
+            }
+        case "setTags":
+            if let jsonString = body["jsonString"] as? String {
+                setTags(jsonString: jsonString)
+            }
+        case "trackEvent":
+            if let type = body["type"] as? String,
+               let jsonString = body["jsonString"] as? String {
+                trackEvent(type: type, jsonString: jsonString)
+            }
+        default:
+            break
+        }
     }
 }
