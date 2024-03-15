883423e8802dfe6aa022ea33505c1c83a3ba9d0d
Jaehyeon Lee <jaehyeon.lee@navercorp.com>
2024-03-15 23:08:36 +0900

    Fix indentation

--- FlareLane/Classes/WebViewController.swift
+++ FlareLane/Classes/WebViewController.swift
@@ -8,61 +8,61 @@
 import WebKit
 
 final class WebViewController: UIViewController, WKNavigationDelegate {
+  
+  private var webView: WKWebView!
+  private var url: URL!
+  
+  init(url: URL) {
+    self.url = url
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func loadView() {
+    super.loadView()
+    webView = WKWebView(frame: self.view.frame)
+    webView.navigationDelegate = self
+    view = webView
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    private var webView: WKWebView!
-    private var url: URL!
-    
-    init(url: URL) {
-        self.url = url
-        super.init(nibName: nil, bundle: nil)
+    if #available(iOS 13.0, *) {
+      navigationItem.leftBarButtonItem = .init(image: UIImage(systemName: "xmark"), style: .done, target: self, action: #selector(closeWebView))
+    } else {
+      navigationItem.leftBarButtonItem = .init(barButtonSystemItem: .done, target: self, action: #selector(closeWebView))
     }
+    navigationController?.navigationBar.prefersLargeTitles = false
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    loadURL(url)
+  }
+  
+  private func loadURL(_ url: URL) {
+    let request = URLRequest(url: url)
+    webView.load(request)
+  }
+  
+  @objc
+  private func closeWebView() {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+    if let url = navigationAction.request.url, url.scheme != "http", url.scheme != "https" {
+      UIApplication.shared.open(url, options: [:])
+      decisionHandler(.cancel)
+      return
     }
-    
-    override func loadView() {
-        super.loadView()
-        webView = WKWebView(frame: self.view.frame)
-        webView.navigationDelegate = self
-        view = webView
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if #available(iOS 13.0, *) {
-            navigationItem.leftBarButtonItem = .init(image: UIImage(systemName: "xmark"), style: .done, target: self, action: #selector(closeWebView))
-        } else {
-            navigationItem.leftBarButtonItem = .init(barButtonSystemItem: .done, target: self, action: #selector(closeWebView))
-        }
-        navigationController?.navigationBar.prefersLargeTitles = false
-        
-        loadURL(url)
-    }
-    
-    private func loadURL(_ url: URL) {
-        let request = URLRequest(url: url)
-        webView.load(request)
-    }
-    
-    @objc
-    private func closeWebView() {
-        dismiss(animated: true, completion: nil)
-    }
-    
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if let url = navigationAction.request.url, url.scheme != "http", url.scheme != "https" {
-            UIApplication.shared.open(url, options: [:])
-            decisionHandler(.cancel)
-            return
-        }
-        decisionHandler(.allow)
-    }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        let host = webView.url?.host ?? ""
-        self.navigationItem.title = host
-    }
+    decisionHandler(.allow)
+  }
+  
+  func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    let host = webView.url?.host ?? ""
+    self.navigationItem.title = host
+  }
 }
 
