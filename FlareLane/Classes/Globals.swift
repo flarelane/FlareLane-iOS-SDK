ba9a904b4378432c6528c0b821bdf7cfa5032afb
MinHyeok Kim <minhyeok4dev@gmail.com>
2025-03-18 14:45:18 +0900

    Set weak for webView

--- FlareLane/Classes/Globals.swift
+++ FlareLane/Classes/Globals.swift
@@ -14,7 +14,7 @@ public enum SdkType: String {
 }
 
 final class Globals {
-  static var sdkVersion = "1.7.3"
+  static var sdkVersion = "1.7.4"
   static var sdkType: SdkType = .native
   static var sdkPlatform = "ios"
 
