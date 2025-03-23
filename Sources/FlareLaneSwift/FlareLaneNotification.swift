8b9a5412971936c127aa6167894dead651d80355
MinHyeok Kim <minhyeok4dev@gmail.com>
2025-03-23 23:19:00 +0900

    a

--- Sources/FlareLaneSwift/FlareLaneNotification.swift
+++ Sources/FlareLaneSwift/FlareLaneNotification.swift
@@ -5,7 +5,8 @@
 //  Copyright © 2021 FlareLabs. All rights reserved.
 //
 
-import Foundation
+import UserNotifications
+import UIKit
 
 @objc open class FlareLaneNotification: NSObject {
   // In case of structure, it is difficult to be compatible with Objective-C
@@ -15,7 +16,7 @@ import Foundation
   public var url: String?
   public var imageUrl: String?
   public var data: Dictionary<String, Any>?
-  
+
   public init(id: String, body: String, title: String?, url: String?, imageUrl: String?, data: Dictionary<String, Any>?) {
     self.id = id
     self.body = body
@@ -25,19 +26,19 @@ import Foundation
     self.url = url == "" ? nil : url
     self.imageUrl = imageUrl == "" ? nil : imageUrl
   }
-  
+
   open override var description: String {
     return "id:\(id)\nbody:\(body)\ntitle:\(String(describing: title))\nurl:\(String(describing: url))\nimageUrl:\(String(describing: imageUrl))\ndata:\(String(describing: data))"
   }
-  
+
   static func getFlareLaneNotificationFromUserInfo(userInfo: [AnyHashable: Any]) -> FlareLaneNotification? {
-    
+
     let isFlareLane = userInfo["isFlareLane"] as? Bool
     if (isFlareLane != true) {
       Logger.error("Not a notification from FlareLane.")
       return nil
     }
-    
+
     guard let aps = userInfo["aps"] as? Dictionary<String, Any>,
           let alert = aps["alert"] as? Dictionary<String, Any>,
           let notificationId = userInfo["notificationId"] as? String,
@@ -45,7 +46,7 @@ import Foundation
             Logger.error("Failed to get FlareLaneNotification: Missing required keys")
             return nil
           }
-    
+
     let notification = FlareLaneNotification(id: notificationId,
                                              body:body,
                                              title:alert["title"] as? String,
@@ -53,27 +54,27 @@ import Foundation
                                              imageUrl: userInfo["imageUrl"] as? String,
                                              data: userInfo["data"] as? Dictionary<String, Any>
     )
-    
+
     return notification
   }
-  
+
   static func getFlareLaneNotificationFromLaunchOptions (launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> FlareLaneNotification? {
     guard let userInfo = launchOptions?[.remoteNotification] as? Dictionary<String, Any>,
           let notification = FlareLaneNotification.getFlareLaneNotificationFromUserInfo(userInfo: userInfo) else {
             return nil
           }
-    
+
     return notification
   }
-  
+
   public static func getFlareLaneNotificationFromUNNotificationContent(_ notificationContent: UNNotificationContent) -> FlareLaneNotification? {
     guard let flarelaneNotification = FlareLaneNotification.getFlareLaneNotificationFromUserInfo(userInfo: notificationContent.userInfo) else {
       return nil
     }
-    
+
     return flarelaneNotification
   }
-  
+
   public func toDictionary() -> [String: Optional<Any>] {
     let dict: [String: Optional<Any>] = [
       "id": id,
@@ -83,7 +84,7 @@ import Foundation
       "imageUrl": imageUrl,
       "data": data
     ]
-    
+
     return dict
   }
 }
