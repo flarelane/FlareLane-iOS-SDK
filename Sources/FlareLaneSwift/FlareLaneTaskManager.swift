a41f4b51d328429a9bbaf7cf238f002645b5a197
MinHyeok Kim <minhyeok4dev@gmail.com>
2025-03-23 23:17:19 +0900

    a

--- Sources/FlareLaneSwift/FlareLaneTaskManager.swift
+++ Sources/FlareLaneSwift/FlareLaneTaskManager.swift
@@ -5,26 +5,28 @@
 //  Created by MinHyeok Kim on 8/14/24.
 //
 
+import Foundation
+
 class FlareLaneTaskManager {
   static let shared = FlareLaneTaskManager()
-  
+
   private var taskQueue = OperationQueue()
   private var isInitialized = false
-  
+
   init() {
     taskQueue.maxConcurrentOperationCount = 1 // Ensure tasks are processed sequentially
     taskQueue.isSuspended = true // Suspend task execution until initialization is complete
   }
-  
+
   func addTaskAfterInit(taskName: String, timeout: TimeInterval = 10.0, _ task: @escaping (_ completion: @escaping () -> Void) -> Void) {
     Logger.verbose("Task added to queue: '\(taskName)'. Queue size after adding: \(taskQueue.operationCount + 1)")
-    
+
     let operation = BlockOperation {
       let semaphore = DispatchSemaphore(value: 0)
       var taskCompleted = false
-      
+
       Logger.verbose("Executing task: '\(taskName)'. Queue size before execution: \(self.taskQueue.operationCount)")
-      
+
       // Execute the task on a background thread
       DispatchQueue.global(qos: .userInitiated).async {
         task {
@@ -33,7 +35,7 @@ class FlareLaneTaskManager {
           semaphore.signal() // Signal that the task is complete
         }
       }
-      
+
       // Set up the timeout
       DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + timeout) {
         if !taskCompleted {
@@ -41,18 +43,18 @@ class FlareLaneTaskManager {
           semaphore.signal() // Signal that the timeout has occurred
         }
       }
-      
+
       semaphore.wait() // Wait for the task or timeout to complete
-      
+
       // Ensure task completion is called even if the semaphore wait fails
       if !taskCompleted {
         Logger.error("Task '\(taskName)' did not complete properly, but semaphore was released.")
       }
     }
-    
+
     taskQueue.addOperation(operation)
   }
-  
+
   func initializeComplete() {
     isInitialized = true
     Logger.verbose("Task queue initialized. Processing queued tasks.")
